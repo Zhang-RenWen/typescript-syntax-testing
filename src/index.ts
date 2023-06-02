33500f04a109acb4243aa3ea0bb2f8df7c84d7d0
zhangrenwen <zhangrenwen0107@gmail.com>
2023-06-02 22:08:33 +0800

    feat(index.ts): add type test

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,18 @@ let c = {
 let d = c.apple * 4;
 
 console.log(d); // 12
+
+// Type 型別
+
+//  any
+let any_a: any = 777;
+let any_b: any = ["danger"];
+let any_c: any = a + b;
+
+//  unknown
+let unknown_a: unknown = 777;
+let unknown_b = unknown_a === 123;
+let unknown_c = unknown_a + 10;
+if (typeof unknown_a === "number") {
+  let unknown_d = unknown_a + 10;
+}
